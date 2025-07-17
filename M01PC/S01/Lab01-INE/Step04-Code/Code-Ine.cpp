b67d1b9e86be7cf9a03bcb24c2ceca6010ad85ec
G-Moll <photocromatic@gmail.com>
2025-07-17 15:45:58 -0600

    EDC PC

--- M01PC/S01/Lab01-INE/Step04-Code/Code-Ine.cpp
+++ M01PC/S01/Lab01-INE/Step04-Code/Code-Ine.cpp
@@ -1,13 +1,48 @@
 #include <iostream>
+#include <thread>
+#include <chrono>
 using namespace std;
+using namespace chrono;
+using namespace this_thread;
+
+void processingTask() {
+    sleep_for( milliseconds( 1700 ) );
+}
+
+void getBallots() {
+    cout << "Recibiendo boletas..." << endl;
+    processingTask();
+    cout << "Boletas checadas..!" << endl;
+}
+void chooseCandidates() {
+    cout << "Escogiendo candidatos..." << endl;
+    processingTask();
+    cout << "Candidatos seleccionados..!" << endl;
+}
+void castVotes() {
+    cout << "Llenar urnas..." << endl;
+    processingTask();
+    cout << "Urnas llenadas..!" << endl;
+}
+void markThumb() {
+    cout << "Sellando pulgar..." << endl;
+    processingTask();
+    cout << "Pulgar sellado..!" << endl;
+}
 
 int main() {
 
-    // Variables/Constantes
     int numCasillaVotante = 10;
+    int numVotante = 2010;
     int numCasillaVisitada = 10;
-    if( numCasillaVotante == numCasillaVisitada ) {
+
+    if( numCasillaVotante == numCasillaVisitada && numVotante >= 1200 && numVotante <= 2500 ) {
         cout << "Si Puede votar" << endl;
+        getBallots();
+        chooseCandidates();
+        castVotes();
+        markThumb();
+        cout << "Voto hecho..!" << endl;
     }
     else {
         cout << "No puede votar" << endl;
